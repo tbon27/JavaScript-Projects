e45453328e6450e4e83cd0e5ecf3952853351704
Tony B <tbongio27@gmail.com>
2021-12-09 17:25:44 -0700

    Created Project7_scope_time_function index.html and main.js

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,35 @@
+var startingNumber = 15; //global!
+
+//================================
+//Working FUNCTION using global variable, declares local variable
+//================================
+function Add() {
+  document.getElementById("Adding").innerHTML = result();
+  function result() {
+    var errorStartValue = 7; //local!!
+    function addOn() {
+      startingNumber += 20;
+    }
+    function error() {
+      errorStartValue += 20;
+    }
+    addOn();
+    error();
+    return startingNumber;
+    return errorStartValue;
+  }
+}
+
+//================================
+// BAD FUNCTION - errorStartValue hasn't been defined here, it is locally defined in Add() function
+//================================
+function BadAdd() {
+  document.getElementById("BadAdding").innerHTML = result();
+  function result() {
+    function error() {
+      errorStartValue += 20;
+    }
+    error();
+    return errorStartValue;
+  }
+}
