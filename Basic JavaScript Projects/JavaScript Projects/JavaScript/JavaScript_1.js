3a0883b5bbd95f12a5778dde3104b41f10fa690d
Tony B <tbongio27@gmail.com>
2021-12-16 10:25:49 -0700

    Updated HTML_1 and JavaScript_1.js
    Course progress

--- Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
@@ -1,3 +1,6 @@
+/*============
+//SWITCH DEMO
+=============*/
 function Genre_Function() {
   var Genre_Output;
   var Genres = document.getElementById("Genre_Input").value;
@@ -28,3 +31,52 @@ function Genre_Function() {
   }
   document.getElementById("Output").innerHTML = Genre_Output;
 }
+
+/*============
+// Get Elements By Class Name Method example
+=============*/
+function Hello_World_Function() {
+  var A = document.getElementsByClassName("Click");
+  A[0].innerHTML = "The text has changed!";
+}
+
+/*============
+// Canvas path demo - beingPath, moveTo, lineTo, stroke, bezierCurveTo
+=============*/
+var path = document.getElementById("pathCanvas");
+var pathDraw = path.getContext("2d"); //returns a drawing context on the canvas, or null if the context identifier is not supported, or the canvas has already been set to a different context mode
+
+pathDraw.beginPath();
+pathDraw.lineWidth = "20";
+pathDraw.strokeStyle = "red"; // red path
+pathDraw.moveTo(0, 75); //move to start point
+pathDraw.lineTo(250, 175); //create "line" to new point
+pathDraw.stroke(); // draw the line
+
+pathDraw.beginPath();
+pathDraw.lineWidth = "100";
+pathDraw.strokeStyle = "blue"; // blue path
+pathDraw.moveTo(350, 20); //move to start point
+pathDraw.lineTo(500, 200); //create "line" to new point
+pathDraw.stroke(); // draw the line
+
+pathDraw.beginPath();
+pathDraw.lineWidth = "50";
+pathDraw.strokeStyle = "yellow"; // yellow path
+pathDraw.moveTo(100, 300); //move to start point
+//ctx.lineTo(150, 530);
+pathDraw.bezierCurveTo(200, 200, 900, 520, 10, 550); //create "bezier curve" to new point
+pathDraw.stroke(); // draw the curved line
+
+/*============
+//  createLinearGradient() method
+=============*/
+var gradient = document.getElementById("gradientCanvas");
+var goGradient = gradient.getContext("2d");
+
+var newGradient = goGradient.createLinearGradient(0, 0, 500, 0);
+newGradient.addColorStop(0, "rgba(0,100,0,1)"); //at colorstop 0 color is..)
+newGradient.addColorStop(1, "rgba(100,0,100,1)"); //at colorstop 1,end, color is..)
+
+goGradient.fillStyle = newGradient;
+goGradient.fillRect(0, 0, 900, 40);
